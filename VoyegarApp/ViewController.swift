0851af85fd757fc88861c3e4734ba38c255548f3
krishna <mailkrishanvir@gmail.com>
2018-05-20 09:41:49 +0530

    second commit

---
+++ VoyegarApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoyegarApp
+//
+//  Created by Rajeev Kumar on 20/05/18.
+//  Copyright © 2018 voyager group limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
